19f75a643e3f0d2836b2530ddb5be4e225fffcf0
Johdasgran <dampig14@gmail.com>
2021-11-24 11:52:14 -0500

    referenciar clase UsuarioController

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,3 +1,59 @@
+const usuarios = [
+    { id: 1, nombre: "Leonardo", apellido: "Montana" },
+    { id: 2, nombre: "Santiago", apellido: "Montana" },
+    { id: 3, nombre: "Nicolas", apellido: "Montana" },
+    { id: 4, nombre: "Ana", apellido: "Montana" }
+]
+
 class UsuarioController {
 
+    obtenerUsuario(req, res) {
+        // console.log(req);
+        let id = parseInt(req.params.id);
+        let objUsu = usuarios.filter(usuario => (usuario.id === id));
+        res.status(200).json(objUsu);
+    }
+
+    registrarUsuario(req, res) {
+        let { nombre, apellido } = req.body;
+        console.table({ nombre, apellido });
+        // let objUser = req.body;
+        // console.log(objUser);
+
+        // usuarios.push({ id: usuarios.length + 1, nombre: nombre, apellido: apellido });
+        // console.table(usuarios);
+
+        // let objUser = req.body;
+        // console.log(objUser.paciente);
+
+        let temp = usuarios.map(user => user.id);
+        let id = Math.max(...temp) + 1;
+        usuarios.push({ id, nombre, apellido });
+
+        res.status(201).send();
+    }
+
+    actualizarUsuario(req, res) {
+        let { id, nombre, apellido } = req.body;
+
+        usuarios.map(objUsuario => {
+            if (objUsuario.id == id) {
+                objUsuario.nombre = nombre;
+                objUsuario.apellido = apellido;
+            }
+        });
+
+        res.status(200).send();
+    }
+
+    borrarUsuario(req, res) {
+        let { id } = req.body;
+        let index = usuarios.map((e) => e.id).indexOf(id);
+        usuarios.splice(index, 1);
+        res.status(200).send();
+    }
+
+
 }
+
+module.exports = UsuarioController;
--- src/routers/usuarioRouter.js
+++ src/routers/usuarioRouter.js
@@ -4,20 +4,21 @@
 const e = require('express');
 const { Router } = require('express');
 
-const usuarios = [
-    { id: 1, nombre: "Leonardo", apellido: "Montana" },
-    { id: 2, nombre: "Santiago", apellido: "Montana" },
-    { id: 3, nombre: "Nicolas", apellido: "Montana" },
-    { id: 4, nombre: "Ana", apellido: "Montana" }
-]
+const UsuarioController = require('../controllers/usuarioController');
+
 
 class UsuarioRouter {
     constructor() {
+
         this.router = Router();
         this.config();
+
     }
 
     config() {
+
+        const objUsuarioC = new UsuarioController();
+
         this.router.get('/usuarios', (req, res) => {
             res.status(200).json(usuarios);
         });
@@ -30,64 +31,16 @@ class UsuarioRouter {
         //     });
         // });
 
-        this.router.get('/usuarios/:id', this.obtenerUsuario);
-        this.router.post('/usuarios', this.registrarUsuario);
-        this.router.put('/usuarios', this.actualizarUsuario);
-        this.router.delete('/usuarios', this.borrarUsuario);
+        this.router.get('/usuarios/:id', objUsuarioC.obtenerUsuario);
+        this.router.post('/usuarios', objUsuarioC.registrarUsuario);
+        this.router.put('/usuarios', objUsuarioC.actualizarUsuario);
+        this.router.delete('/usuarios', objUsuarioC.borrarUsuario);
 
     }
 
     //"#" para volver privado los metodos
 
 
-    obtenerUsuario(req, res) {
-        // console.log(req);
-        let id = parseInt(req.params.id);
-        let objUsu = usuarios.filter(usuario => (usuario.id === id));
-        res.status(200).json(objUsu);
-    }
-
-    registrarUsuario(req, res) {
-        let { nombre, apellido } = req.body;
-        console.table({ nombre, apellido });
-        // let objUser = req.body;
-        // console.log(objUser);
-
-        // usuarios.push({ id: usuarios.length + 1, nombre: nombre, apellido: apellido });
-        // console.table(usuarios);
-
-        // let objUser = req.body;
-        // console.log(objUser.paciente);
-
-        let temp = usuarios.map(user => user.id);
-        let id = Math.max(...temp) + 1;
-        usuarios.push({ id, nombre, apellido });
-
-        res.status(201).send();
-    }
-
-    actualizarUsuario(req, res) {
-        let { id, nombre, apellido } = req.body;
-
-        usuarios.map(objUsuario => {
-            if (objUsuario.id == id) {
-                objUsuario.nombre = nombre;
-                objUsuario.apellido = apellido;
-            }
-        });
-
-        res.status(200).send();
-    }
-
-    borrarUsuario(req, res) {
-        let { id } = req.body;
-        let index = usuarios.map((e) => e.id).indexOf(id);
-        usuarios.splice(index, 1);
-
-
-
-        res.status(200).send();
-    }
 
 }
 
